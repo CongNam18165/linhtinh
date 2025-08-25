8119aea76a01ccc2d2eb2be0188585f3c3ebba68
unknown <namnc1@vnpay.vn>
2025-08-25 23:39:14 +0700

    update

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
+import { CommonModule, } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
-export class App {
+export class App implements OnInit{
   protected readonly title = signal('chucEmLinhNn');
+
+  public message: string = '';
+  public isClick: boolean = true;
+  ngOnInit(): void {
+    console.log('55', this.message);
+  }
+
+  sendWishes() {
+    this.isClick= false;
+    this.message = `Hôm nay e đã vất vả rồi.Chúc em Thảo Linh Phú Thọ ngủ thật ngon 8386 nhé 😴✨.`;
+  }
+
 }
